5adbd03c7c3b944fd2e0264f2bc728da0274819b
David Gee <david@arsonistgopher.com>
2019-03-25 16:31:55 +0000

    Added installer, LICENSE and README.
    Signed-off-by: David Gee, networkbootstrap

---
+++ README.md
@@ -0,0 +1,49 @@
+## ZTP Manager
+
+ZTP Manager is a wrapper for the [ISC DHCP Server](https://www.isc.org/downloads/dhcp/) which enables you to do zero-touch provisioning for network devices. The wrapper has a HTTP JSON API for ease of configuration.
+
+This wrapper not only provides automatic configuration generation for the `/etc/dhcp/dhcpd.conf` file, but also the `/etc/default/isc-dhcp-server` file and initial golden configurations for the devices. 
+
+Take a fresh installation of Ubuntu LTS (16.04 / 18.04) and make sure everything is updated.
+
+## Installation
+
+Installation is super simple. You are required to know the physical interface you wish to servce DHCP on and the server URL for the API. Be sure to change the below line to reflect this. Both configuration items can be found at the end of the line.
+
+`curl -sSL https://raw.githubusercontent.com/networkbootstrap/ztpmanager/master/install.sh | bash -s -- --url=ztp.simpledemo.net --iface=ens34`
+
+## Usage
+
+If you take a look at the `config.toml` file and you'll see one host already configured. It's possible to initially create hosts here as well as through the JSON API.
+
+__Create Hosts__
+
+curl -X POST \
+  -H 'Content-Type: application/json' \
+  -d '{
+    "ethernetaddress": "00:0c:29:4d:3d:cd",
+    "fixedipaddress": "192.168.50.101",
+    "hostname": "demo02",
+    "vendor": "junos"
+    }' \
+   192.168.10.226:1323/hosts
+
+__Delete Hosts__
+
+curl -X DELETE -H 'Content-Type: application/json' 192.168.10.226:1323/hosts/<HOST_IP>
+
+__Get Hosts__
+
+curl -X GET http://192.168.10.226:1323/hosts
+
+__Get Individual Hosts__
+
+curl -X GET http://192.168.10.226:1323/hosts/<HOST_IP>
+
+__Save__
+
+curl -X POST -H 'Content-Type: application/json' 192.168.10.226:1323/save
+
+
+
+ 
---
+++ install.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+echo 'Installing ZTP server manager framework and ISC-DHCP-SERVER'
+
+ARCH=$(arch)
+DEBTEST=`lsb_release -a 2> /dev/null | grep Distributor | awk '{print $3}'`
+
+sudo apt-get install -y isc-dhcp-server=4.3.5-3ubuntu7
+sudo apt-get install -y git
+
+URL="ztp.localhost"
+
+setup_args() {
+  for i in "$@"
+    do
+      case $i in
+ 	  # Server URL location.
+          --url=*)
+          URL="${i#*=}"
+          shift
+          ;;
+	  --iface=*)
+          IFACE="${i#*=}"
+	  shift
+	  ;;
+          *)
+          # unknown option
+          ;;
+      esac
+    done
+}
+
+setup_args $@
+
+if [ "$ARCH" != "x86_64" ]; then
+  echo "Unsupported architecture. Please use a 64-bit OS! Aborting!"
+  exit 2
+fi
+
+echo "Architecture is x86_64. Good!"
+
+if [ -n "$DEBTEST" ]; then
+  TYPE="debs"
+  echo "*** Detected Distro is ${DEBTEST} ***"
+  SUBTYPE=`lsb_release -a 2>&1 | grep Codename | grep -v "LSB" | awk '{print $2}'`
+  echo "*** Detected flavor ${SUBTYPE} ***"
+
+  if [ "$SUBTYPE" != 'trusty' && "$SUBTYPE" != 'xenial' && "$SUBTYPE" != 'bionic' ]; then
+    echo "Unsupported ubuntu flavor ${SUBTYPE}. Please use 16.04 (xenial) or 18.04 (bionic) as base system!"
+    exit 2
+  fi
+
+fi
+
+
+# Now we download the binary using CURL and do an MD5 on it to ensure it's the same. Users can of course build from source if they require and validate the MD5.
+
+git clone https://github.com/networkbootstrap/ztpmanager.git
+cd ztpmanager
+# curl -sS -k -o ztpmanager https://raw.githubusercontent.com/networkbootstrap/ztpmanager/master/ztpmanager
+# curl -sS -k -o ztpmanager.md5 https://raw.githubusercontent.com/networkbootstrap/ztpmanager/master/ztpmanager.md5
+# curl -sS -k -o config.toml https://raw.githubusercontent.com/networkbootstrap/ztpmanager/master/config.toml
+# wget -r https://raw.githubusercontent.com/networkbootstrap/ztpmanager/master/templates
+
+md5sum ztpmanager | awk '{print $1}' > calculated.md5
+file1="calculated.md5"
+file2="ztpmanager.md5"
+
+if ! cmp "$file1" "$file2"; then
+echo "MD5 comparison failed, exiting..."
+exit 1
+fi
+
+echo "MD5 file comparison passed..."
+
+sed -i "s/  ServerURL = \"localhost\"/  ServerURL = \"$URL\"/g" config.toml
+sed -i "s/  DHCPIface = \"ens34\"/  DHCPIface = \"$IFACE\"/g" config.toml
+
+echo "Restarting ISC server..."
+sudo service isc-dhcp-server restart
+echo "Restarted ISC server..."
+
+chmod a+x ztpmanager
+
+echo "Creating default config and image directories..."
+mkdir configs
+mkdir images
+echo "Created default config and image directories..."
+
+echo "Starting service"
+sudo ./ztpmanager -config config.toml &
